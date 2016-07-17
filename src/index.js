416bb19d3eaf16099a0f57e068428e12b90f4d9f
lizheming <secretlzm007@gmail.com>
2016-07-17 18:35:36 +0800

    plugin init

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import Plugin from 'stc-plugin';
+
+export default class i18nPlugin extends Plugin {
+  /**
+   * run
+   */
+  async run() {
+
+  }
+
+  /**
+   * update
+   */
+  async update() {
+
+  }
+
+  /**
+   * use cluster
+   */
+  static cluster() {
+    return false;
+  }
+
+  /**
+   * use cache
+   */
+  static cache() {
+    return false;
+  }
+}
